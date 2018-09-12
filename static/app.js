004d839640845cb02db36547749b0aaec0008f9d
chiayenhung <hodalalala@gmail.com>
2018-09-11 23:55:51 -0700

    complete calculation

--- static/app.js
+++ static/app.js
@@ -50,34 +50,85 @@
       }
     ];
 
-    const handleOnKeyUp = (e, rowNum) => {
-      console.log(e.currentTarget.value, rowNum);
+    const data = [];
+
+    const getSubTotal = (index, reverse = false) => {
+      if (!data[index] || !data[index].length) {
+        return 0;
+      }
+      return data[index].reduce((current, name) => {
+        const optionIndex = optionConfig.findIndex((config) => {
+          return config.name === name;
+        });
+        if (optionIndex === -1) {
+          return current;
+        }
+        return (reverse ? optionIndex + 1 : 4 - optionIndex) + current;
+      }, 0);
+    };
+
+    const renderSubTotals = () => {
+      tableConfig.forEach((config, index) => {
+        const {
+          reverse
+        } = config || {};
+        const subtotal = getSubTotal(index, reverse);
+        $(".table" + index).text(subtotal);
+      });
+    };
+
+    const getTotal = () => {
+      let total = 0;
+      tableConfig.forEach((config, index) => {
+        const {
+          reverse
+        } = config || {};
+        const subtotal = getSubTotal(index, reverse);
+        total += subtotal;
+      });
+      return total;
+    };
+
+    const renderTotal = () => {
+      const total = getTotal();
+      $(".total").text(total);
+    };
+
+    const handleOnKeyUp = (e, rowNum, rowConfig, rowIndex) => {
+      const rowIndexNum = tableConfig.indexOf(rowConfig);
+      if (!data[rowIndexNum]) {
+        data[rowIndexNum] = [];
+      }
+      data[rowIndexNum][rowIndex] = e.currentTarget.value;
+      renderSubTotals();
+      renderTotal();
     };
 
     const getHeaders = (tableNum) => {
-      return "<thead><tr><th scope=\"col\">#</th><th scope=\"col\">Answer</th><th><span>Sub total:</span><span class=\"table " + tableNum +"\"></span></th></tr></thead>";
+      return "<thead><tr><th scope=\"col\">#</th><th scope=\"col\">Answer</th><th><span>Sub total:</span><span class=\"table" + tableNum +"\"></span></th></tr></thead>";
     };
 
-    const getOption = (config, index, rowNum) => {
+    const getOption = (config, index, rowNum, rowConfig, rowIndex) => {
       const {
         name,
         label
       } = config || {};
       const id = `${name}-${rowNum}`;
       const option = $("<div class=\"col-md-2 mb-2\"><label for=\"" + id + "\">" + label + "</label><input type=\"radio\" class=\"form-control\" id=\"" + id + "\" placeholder=\"First name\" value=\"" + name + "\" name=" + rowNum + "></div>");
-      option.find("input").change((e) => handleOnKeyUp(e, rowNum));
+      option.find("input").change((e) => handleOnKeyUp(e, rowNum, rowConfig, rowIndex));
       return option;
     };
 
-    const getOptions = (rowNum) => {
+    const getOptions = (rowNum, rowIndex, rowConfig) => {
       return optionConfig.map((config, index) => {
-        return getOption(config, index, rowNum);
+        return getOption(config, index, rowNum, rowConfig, rowIndex);
       });
     };
 
-    const getRow = (index) => {
-      const row = $("<tr><th scope=\"row\">" + index + "</th><td class=\"form-row\"></td><td></td></tr>");
-      row.find(".form-row").append(getOptions(index));
+    const getRow = (start, index, rowConfig) => {
+      const rowNum = start + index;
+      const row = $("<tr><th scope=\"row\">" + rowNum + "</th><td class=\"form-row\"></td><td></td></tr>");
+      row.find(".form-row").append(getOptions(rowNum, index, rowConfig));
       return row;
     };
 
@@ -87,7 +138,7 @@
         rowNum = 0
       } = config || {};
       for(let i = 0; i < rowNum; i++) {
-        rows.push(getRow(start + i, config));
+        rows.push(getRow(start, i, config));
       }
       return rows;
     };
